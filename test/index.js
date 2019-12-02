ef111c188129072610cca17c57ed9d90b159d19c
Ivan Nikolić <niksy5@gmail.com>
2019-12-02 12:16:31 +0100

    Upgrade package

--- test/index.js
+++ test/index.js
@@ -8,49 +8,47 @@ const stylelint = require('stylelint');
 const pify = require('pify');
 
 /**
- * @param  {String} file
+ * @param  {string} file
  * @param  {Object} config
  *
- * @return {Promise}
+ * @returns {Promise}
  */
-function runStylelint(file, config) {
-	return pify(fs.readFile)(path.join(__dirname, file), 'utf8')
-		.then((code) =>
-			stylelint.lint({
-				code,
-				config
-			})
-		)
-		.catch((err) => {
-			throw err;
+async function runStylelint(file, config) {
+	const code = await pify(fs.readFile)(path.join(__dirname, file), 'utf8');
+	try {
+		return await stylelint.lint({
+			code,
+			config
 		});
+	} catch (error) {
+		throw error;
+	}
 }
 
 /**
- * @param  {String} configFile
+ * @param  {string} configFile
  *
- * @return {Promise}
+ * @returns {Promise<Array>}
  */
-function validateConfiguration(configFile) {
-	return stylelint
-		.lint({
+async function validateConfiguration(configFile) {
+	try {
+		const data = await stylelint.lint({
 			code: '',
 			config: require(configFile)
-		})
-		.then(
-			(data) =>
-				[].concat(
-					data.results[0].deprecations,
-					data.results[0].invalidOptionWarnings
-				),
-			() => []
+		});
+		return [].concat(
+			data.results[0].deprecations,
+			data.results[0].invalidOptionWarnings
 		);
+	} catch (error) {
+		return [];
+	}
 }
 
 /**
  * @param  {Object[]} errors
  *
- * @return {String[]}
+ * @returns {Array<string>}
  */
 function mapErrors(errors) {
 	return errors.map((warning) => warning.rule);
@@ -64,45 +62,43 @@ describe('Default config', function() {
 		assert.ok(isPlainObject(config.rules));
 	});
 
-	it('should not have invalid options or deprecation warnings', function() {
-		return validateConfiguration('../').then((errors) => {
-			assert.equal(errors.length, 0);
-		});
+	it('should not have invalid options or deprecation warnings', async function() {
+		const errors = await validateConfiguration('../');
+		assert.equal(errors.length, 0);
 	});
 
-	it('should return proper validation errors for linted code', function() {
-		return runStylelint('./fixtures/default.config.css', {
-			extends: require.resolve('../')
-		})
-			.then((data) => {
-				const errors = mapErrors(data.results[0].warnings);
-				assert.notEqual(errors.indexOf('number-leading-zero'), -1);
-				assert.notEqual(
-					errors.indexOf('declaration-property-value-blacklist'),
-					-1
-				);
-				assert.notEqual(
-					errors.indexOf('declaration-colon-space-after'),
-					-1
-				);
-				assert.notEqual(
-					errors.indexOf('plugin/selector-pseudo-class-lvhfa'),
-					-1
-				);
-				assert.notEqual(
-					errors.indexOf('plugin/value-list-box-shadow-inset-first'),
-					-1
-				);
-				assert.notEqual(
-					errors.indexOf('plugin/number-z-index-constraint'),
-					-1
-				);
-				assert.notEqual(errors.indexOf('order/properties-order'), -1);
-				return data;
-			})
-			.catch((err) => {
-				assert.ifError(err);
+	it('should return proper validation errors for linted code', async function() {
+		try {
+			const data = await runStylelint('./fixtures/default.config.css', {
+				extends: require.resolve('../')
 			});
+			const errors = mapErrors(data.results[0].warnings);
+			assert.notEqual(errors.indexOf('number-leading-zero'), -1);
+			assert.notEqual(
+				errors.indexOf('declaration-property-value-blacklist'),
+				-1
+			);
+			assert.notEqual(
+				errors.indexOf('declaration-colon-space-after'),
+				-1
+			);
+			assert.notEqual(
+				errors.indexOf('plugin/selector-pseudo-class-lvhfa'),
+				-1
+			);
+			assert.notEqual(
+				errors.indexOf('plugin/value-list-box-shadow-inset-first'),
+				-1
+			);
+			assert.notEqual(
+				errors.indexOf('plugin/number-z-index-constraint'),
+				-1
+			);
+			assert.notEqual(errors.indexOf('order/properties-order'), -1);
+			return data;
+		} catch (error) {
+			assert.ifError(error);
+		}
 	});
 });
 
@@ -114,35 +110,31 @@ describe('SCSS config', function() {
 		assert.ok(isPlainObject(config.rules));
 	});
 
-	it('should not have invalid options or deprecation warnings', function() {
-		return validateConfiguration('../scss').then((errors) => {
-			assert.equal(errors.length, 0);
-		});
+	it('should not have invalid options or deprecation warnings', async function() {
+		const errors = await validateConfiguration('../scss');
+		assert.equal(errors.length, 0);
 	});
 
-	it('should return proper validation errors for linted code', function() {
-		return runStylelint('./fixtures/scss.config.scss', {
-			extends: require.resolve('../scss')
-		})
-			.then((data) => {
-				const errors = mapErrors(data.results[0].warnings);
-				assert.notEqual(errors.indexOf('scss/at-function-pattern'), -1);
-				assert.notEqual(
-					errors.indexOf(
-						'scss/at-import-partial-extension-blacklist'
-					),
-					-1
-				);
-				assert.notEqual(errors.indexOf('at-rule-blacklist'), -1);
-				assert.equal(errors.indexOf('at-rule-no-unknown'), -1);
-				assert.equal(
-					errors.indexOf('block-closing-brace-newline-after'),
-					-1
-				);
-				return data;
-			})
-			.catch((err) => {
-				assert.ifError(err);
+	it('should return proper validation errors for linted code', async function() {
+		try {
+			const data = await runStylelint('./fixtures/scss.config.scss', {
+				extends: require.resolve('../scss')
 			});
+			const errors = mapErrors(data.results[0].warnings);
+			assert.notEqual(errors.indexOf('scss/at-function-pattern'), -1);
+			assert.notEqual(
+				errors.indexOf('scss/at-import-partial-extension-blacklist'),
+				-1
+			);
+			assert.notEqual(errors.indexOf('at-rule-blacklist'), -1);
+			assert.equal(errors.indexOf('at-rule-no-unknown'), -1);
+			assert.equal(
+				errors.indexOf('block-closing-brace-newline-after'),
+				-1
+			);
+			return data;
+		} catch (error) {
+			assert.ifError(error);
+		}
 	});
 });
