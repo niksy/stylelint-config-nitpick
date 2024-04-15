0f92c8cfcd3a8aeeaf61a18b59c9ca11295c8dc2
Ivan Nikolić <niksy5@gmail.com>
2024-04-15 09:34:13 +0200

    Groups Sass stylistic rules

--- scss.js
+++ scss.js
@@ -3,27 +3,14 @@ export default {
 	customSyntax: 'postcss-scss',
 	rules: {
 		'scss/at-each-key-value-single-line': [true, { severity: 'warning' }],
-		'scss/at-else-closing-brace-newline-after': 'always-last-in-chain',
-		'scss/at-else-closing-brace-space-after': [
-			'always-intermediate',
-			{ severity: 'warning' }
-		],
-		'scss/at-else-empty-line-before': ['never', { severity: 'warning' }],
-		'scss/at-else-if-parentheses-space-before': ['always'],
 		'scss/at-extend-no-missing-placeholder': true,
 		'scss/at-function-pattern': [/^[\d_a-z-]+$/],
 		'scss/at-function-named-arguments': [
 			'always',
 			{ ignore: ['single-argument'], severity: 'warning' }
 		],
-		'scss/at-function-parentheses-space-before': 'always',
 		'function-no-unknown': null,
 		'scss/function-no-unknown': [true, { severity: 'warning' }],
-		'scss/at-if-closing-brace-newline-after': 'always-last-in-chain',
-		'scss/at-if-closing-brace-space-after': [
-			'always-intermediate',
-			{ severity: 'warning' }
-		],
 		'scss/at-if-no-null': null,
 		'scss/at-import-partial-extension': 'never',
 		'scss/at-mixin-argumentless-call-parentheses': [
@@ -35,15 +22,11 @@ export default {
 			'always',
 			{ ignore: ['single-argument'], severity: 'warning' }
 		],
-		'scss/at-mixin-parentheses-space-before': 'always',
 		'at-rule-no-unknown': null,
 		'scss/at-rule-no-unknown': [true, { severity: 'warning' }],
 		'scss/at-rule-conditional-no-parentheses': null,
 		'scss/at-use-no-unnamespaced': null,
 		'at-rule-disallowed-list': [['debug'], { severity: 'warning' }],
-		'scss/dollar-variable-colon-newline-after': null,
-		'scss/dollar-variable-colon-space-after': 'always',
-		'scss/dollar-variable-colon-space-before': 'never',
 		'scss/dollar-variable-empty-line-before': null,
 		'scss/dollar-variable-no-missing-interpolation': true,
 		'scss/dollar-variable-pattern': [/^[\d_a-z-]+$/],
@@ -71,9 +54,6 @@ export default {
 		'scss/declaration-nested-properties': 'never',
 		'scss/declaration-nested-properties-no-divided-groups': null,
 		'scss/media-feature-value-dollar-variable': null,
-		'scss/operator-no-newline-after': true,
-		'scss/operator-no-newline-before': true,
-		'scss/operator-no-unspaced': true,
 		'scss/partial-no-import': null,
 		'scss/selector-no-redundant-nesting-selector': [
 			true,
@@ -85,18 +65,6 @@ export default {
 		'scss/no-duplicate-dollar-variables': true,
 		'scss/no-dollar-variables': null,
 		'scss/no-global-function-names': null,
-		'at-rule-empty-line-before': [
-			'always',
-			{
-				except: ['blockless-after-blockless'],
-				ignore: [
-					'blockless-after-blockless',
-					'after-comment',
-					'inside-block'
-				],
-				ignoreAtRules: ['if', 'else']
-			}
-		],
 		'no-duplicate-selectors': null,
 		'annotation-no-unknown': [
 			true,
@@ -116,6 +84,40 @@ export default {
 			['scss', 'sass'],
 			{ severity: 'warning' }
 		],
-		'scss/no-unused-private-members': null
+		'scss/no-unused-private-members': null,
+
+		// Stylistic rules
+		'at-rule-empty-line-before': [
+			'always',
+			{
+				except: ['blockless-after-blockless'],
+				ignore: [
+					'blockless-after-blockless',
+					'after-comment',
+					'inside-block'
+				],
+				ignoreAtRules: ['if', 'else']
+			}
+		],
+		'scss/at-else-closing-brace-newline-after': 'always-last-in-chain',
+		'scss/at-else-closing-brace-space-after': [
+			'always-intermediate',
+			{ severity: 'warning' }
+		],
+		'scss/at-else-empty-line-before': ['never', { severity: 'warning' }],
+		'scss/at-else-if-parentheses-space-before': ['always'],
+		'scss/at-function-parentheses-space-before': 'always',
+		'scss/at-if-closing-brace-newline-after': 'always-last-in-chain',
+		'scss/at-if-closing-brace-space-after': [
+			'always-intermediate',
+			{ severity: 'warning' }
+		],
+		'scss/at-mixin-parentheses-space-before': 'always',
+		'scss/dollar-variable-colon-newline-after': null,
+		'scss/dollar-variable-colon-space-after': 'always',
+		'scss/dollar-variable-colon-space-before': 'never',
+		'scss/operator-no-newline-after': true,
+		'scss/operator-no-newline-before': true,
+		'scss/operator-no-unspaced': true
 	}
 };
