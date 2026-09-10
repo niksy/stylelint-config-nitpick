8d84f0a6172717bc5b6f614d49b1b8b91b8d9acb
Ivan Nikolić <niksy5@gmail.com>
2026-09-10 17:37:37 +0200

    Update tests

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 import { promises as fs } from 'node:fs';
-import assert from 'node:assert';
+import assert from 'node:assert/strict';
 import { fileURLToPath } from 'node:url';
 import isPlainObject from 'lodash/isPlainObject.js';
 import stylelint from 'stylelint';
@@ -33,10 +33,7 @@ async function validateConfiguration(config) {
 			code: '',
 			config: config
 		});
-		return [].concat(
-			data.results[0].deprecations,
-			data.results[0].invalidOptionWarnings
-		);
+		return [].concat(data.results[0].deprecations, data.results[0].invalidOptionWarnings);
 	} catch (error) {
 		return [];
 	}
@@ -67,31 +64,14 @@ describe('Default config', function () {
 
 	it('should return proper validation errors for linted code', async function () {
 		try {
-			const { results } = await runStylelint(
-				'./fixtures/default.config.css',
-				{
-					extends: fileURLToPath(
-						new URL('../index.js', import.meta.url)
-					)
-				}
-			);
+			const { results } = await runStylelint('./fixtures/default.config.css', {
+				extends: fileURLToPath(new URL('../index.js', import.meta.url))
+			});
 			const errors = mapErrors(results[0].warnings);
-			assert.notEqual(
-				errors.indexOf('declaration-property-value-disallowed-list'),
-				-1
-			);
-			assert.notEqual(
-				errors.indexOf('plugin/selector-pseudo-class-lvhfa'),
-				-1
-			);
-			assert.notEqual(
-				errors.indexOf('plugin/value-list-box-shadow-inset-first'),
-				-1
-			);
-			assert.notEqual(
-				errors.indexOf('plugin/number-z-index-constraint'),
-				-1
-			);
+			assert.notEqual(errors.indexOf('declaration-property-value-disallowed-list'), -1);
+			assert.notEqual(errors.indexOf('plugin/selector-pseudo-class-lvhfa'), -1);
+			assert.notEqual(errors.indexOf('plugin/value-list-box-shadow-inset-first'), -1);
+			assert.notEqual(errors.indexOf('plugin/number-z-index-constraint'), -1);
 			assert.notEqual(errors.indexOf('order/properties-order'), -1);
 		} catch (error) {
 			assert.ifError(error);
@@ -115,28 +95,15 @@ describe('SCSS config', function () {
 
 	it('should return proper validation errors for linted code', async function () {
 		try {
-			const { results } = await runStylelint(
-				'./fixtures/scss.config.scss',
-				{
-					extends: fileURLToPath(
-						new URL('../scss.js', import.meta.url)
-					)
-				}
-			);
+			const { results } = await runStylelint('./fixtures/scss.config.scss', {
+				extends: fileURLToPath(new URL('../scss.js', import.meta.url))
+			});
 			const errors = mapErrors(results[0].warnings);
 			assert.notEqual(errors.indexOf('scss/at-function-pattern'), -1);
-			assert.notEqual(
-				errors.indexOf(
-					'scss/at-import-partial-extension-disallowed-list'
-				),
-				-1
-			);
+			assert.notEqual(errors.indexOf('scss/at-import-partial-extension-disallowed-list'), -1);
 			assert.notEqual(errors.indexOf('at-rule-disallowed-list'), -1);
 			assert.equal(errors.indexOf('at-rule-no-unknown'), -1);
-			assert.equal(
-				errors.indexOf('block-closing-brace-newline-after'),
-				-1
-			);
+			assert.equal(errors.indexOf('block-closing-brace-newline-after'), -1);
 		} catch (error) {
 			assert.ifError(error);
 		}
